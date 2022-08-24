446ac6599b0d825682242a9f8f2e6dd3b59d0f17
Feli Caeiro <felicaeiro2@gmail.com>
2022-08-24 20:00:44 -0300

    funcionalidad y traduccion de contacto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import "./Contact.css"
+import React from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-    return (
-        <div class="Contact">
-            <div class="Contact-container">
-                <form method="POST" >
-                    <label class="Contact-detail">
-                        <span >Your name</span>
-                        <input type="text" name="name" placeholder="Joe Bloggs"
-                        />
-                    </label>
-                    <label class="Contact-detail">
-                        <span >Email address</span>
-                        <input name="email" type="email" placeholder="joe.bloggs@example.com" required/>
-                    </label>
-                    <label class="Contact-detail">
-                        <span >Message</span>
-                        <textarea name="message" rows="3" placeholder="Tell us what you're thinking about..."/>
-                    </label>
-                    <div class="Contact-button">
-                        <button class="btn" type="submit">
-                            Contact Us
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        'service_jvpz93g',
+        'template_5gf7yok',
+        e.target,
+        'qKN3lKyzYHg0cRgvh'
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    e.target.reset();
+  };
+  return (
+    <div class="Contact">
+      <div class="Contact-container">
+        <form method="POST" onSubmit={sendEmail}>
+          <label class="Contact-detail">
+            <span>Tu nombre</span>
+            <input type="text" name="name" required />
+          </label>
+          <label class="Contact-detail">
+            <span>Email</span>
+            <input name="email" type="email" required />
+          </label>
+          <label class="Contact-detail">
+            <span>Mensaje</span>
+            <textarea name="message" rows="3" required />
+          </label>
+          <div class="Contact-button">
+            <button class="btn" type="submit">
+              Enviar comentario
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
