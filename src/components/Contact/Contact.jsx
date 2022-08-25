aafd7988e58f77cab69ceaab446836251ca19eee
Carlos7847 <carlitosmp1597g@gmail.com>
2022-08-25 08:59:46 -0500

    se agrega funcionalidad y alerta

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import "./Contact.css"
+import emailjs from "@emailjs/browser"
+import Swal from 'sweetalert2'
 
 export default function Contact() {
+
+    const successAlert = () => {
+        Swal.fire({
+            html: '<b className="alerta">Contacto enviado</b>',
+            icon: 'success',
+            timer: '5000',
+            timerProgressBar: true,
+            })
+        }
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm("service_jvpz93g", "template_5gf7yok", e.target, "qKN3lKyzYHg0cRgvh")
+            .then(res => {
+                console.log(res)
+                return successAlert()
+            })
+            // .catch(err => console.log(err))
+        e.target.reset()
+    }
     return (
         <div class="Contact">
             <div class="Contact-container">
-                <form method="POST" >
+                <button onClick={e=>successAlert(e)}>alert</button>
+                <form  onSubmit={e=>sendEmail(e)}>
                     <label class="Contact-detail">
                         <span >Your name</span>
                         <input type="text" name="name" placeholder="Joe Bloggs"
