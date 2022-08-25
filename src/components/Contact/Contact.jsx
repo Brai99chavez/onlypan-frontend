088de4f3263d7dd2af7982e0405264d55222a004
Feli Caeiro <felicaeiro2@gmail.com>
2022-08-25 11:11:29 -0300

    traducir contacto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,53 +1,66 @@
-import React from 'react'
-import "./Contact.css"
-import emailjs from "@emailjs/browser"
-import Swal from 'sweetalert2'
+import React from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 
 export default function Contact() {
-
-    const successAlert = () => {
-        Swal.fire({
-            html: '<b className="alerta">Contacto enviado</b>',
-            icon: 'success',
-            timer: '5000',
-            timerProgressBar: true,
-            })
-        }
-    const sendEmail = (e) => {
-        e.preventDefault()
-        emailjs.sendForm("service_jvpz93g", "template_5gf7yok", e.target, "qKN3lKyzYHg0cRgvh")
-            .then(res => {
-                console.log(res)
-                return successAlert()
-            })
-            // .catch(err => console.log(err))
-        e.target.reset()
-    }
-    return (
-        <div class="Contact">
-            <div class="Contact-container">
-                <button onClick={e=>successAlert(e)}>alert</button>
-                <form  onSubmit={e=>sendEmail(e)}>
-                    <label class="Contact-detail">
-                        <span >Your name</span>
-                        <input type="text" name="name" placeholder="Joe Bloggs"
-                        />
-                    </label>
-                    <label class="Contact-detail">
-                        <span >Email address</span>
-                        <input name="email" type="email" placeholder="joe.bloggs@example.com" required/>
-                    </label>
-                    <label class="Contact-detail">
-                        <span >Message</span>
-                        <textarea name="message" rows="3" placeholder="Tell us what you're thinking about..."/>
-                    </label>
-                    <div class="Contact-button">
-                        <button class="btn" type="submit">
-                            Contact Us
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  const successAlert = () => {
+    Swal.fire({
+      html: '<b className="alerta">Contacto enviado</b>',
+      icon: 'success',
+      timer: '5000',
+      timerProgressBar: true,
+    });
+  };
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        'service_jvpz93g',
+        'template_5gf7yok',
+        e.target,
+        'qKN3lKyzYHg0cRgvh'
+      )
+      .then((res) => {
+        console.log(res);
+        return successAlert();
+      });
+    // .catch(err => console.log(err))
+    e.target.reset();
+  };
+  return (
+    <div class="Contact">
+      <div class="Contact-container">
+        <form onSubmit={(e) => sendEmail(e)}>
+          <label class="Contact-detail">
+            <span>Tu nombre</span>
+            <input type="text" name="name" placeholder="Juan Perez" required />
+          </label>
+          <label class="Contact-detail">
+            <span>Email</span>
+            <input
+              name="email"
+              type="email"
+              placeholder="juan.perez@ejemplo.com"
+              required
+            />
+          </label>
+          <label class="Contact-detail">
+            <span>Mensaje</span>
+            <textarea
+              required
+              name="message"
+              rows="3"
+              placeholder="Tell us what you're thinking about..."
+            />
+          </label>
+          <div class="Contact-button">
+            <button class="btn" type="submit">
+              Enviar comentario
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
